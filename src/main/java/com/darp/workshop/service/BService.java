985efa9b862395440a6c73742b3720f1313a0c73
SrUltraLord <davidrey8822@gmail.com>
2025-11-14 11:13:08 -0500

    feat: add product service structure

---
+++ src/main/java/com/darp/workshop/service/BService.java
@@ -0,0 +1,16 @@
+package com.darp.workshop.service;
+
+import com.darp.workshop.model.Book;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BService {
+    public Book fetchBookInfo() {
+        return Book.builder()
+                .title("Cumbres Borrascosas")
+                .author("Emily Brontë")
+                .isbn("978-84-376-0494-7")
+                .publishedYear(1847)
+                .build();
+    }
+}
