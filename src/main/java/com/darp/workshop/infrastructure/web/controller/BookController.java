31411d528ae0a73f25e369f4d9065377db986e84
SrUltraLord <davidrey8822@gmail.com>
2025-11-14 11:48:40 -0500

    chore: improve logging in books controller

--- src/main/java/com/darp/workshop/infrastructure/web/controller/BookController.java
+++ src/main/java/com/darp/workshop/infrastructure/web/controller/BookController.java
@@ -6,11 +6,13 @@ import com.darp.workshop.infrastructure.web.dto.response.BookResponseDto;
 import com.darp.workshop.infrastructure.web.mapper.BookDtoMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
@@ -22,6 +24,8 @@ public class BookController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<BookResponseDto> createBook(@Valid @RequestBody CreateBookRequestDto requestDto) {
+        log.info("Received request to create book: {}", requestDto);
+
         return Mono.just(requestDto)
                 .map(dtoMapper::toDomain)
                 .flatMap(bookUseCase::createBook)
@@ -30,6 +34,8 @@ public class BookController {
 
     @GetMapping
     public Flux<BookResponseDto> getAllBooks() {
+        log.info("Received request to get all books");
+
         return bookUseCase.getAllBooks()
                 .map(dtoMapper::toResponseDto);
     }
